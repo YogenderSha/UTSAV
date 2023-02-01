1a1af1afb69d35b69ea11d79d8e1010b6990532a
nitin <nimble2905@gmail.com>
2023-02-01 09:27:00 +0530

    some changes in about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,15 +5,15 @@ import logo from './hero.jpg';
 export default function About() {
   return (
     <>
-    <section className='flex flex-col align-middle md:flex-row w-[95%] mx-auto md:h-[60vh] bg-gradient-to-t from-gray-800 to-gray-900'>
-        <div className='mx-auto block md:hidden my-2'><span className='text-white font-semibold font-sans text-3xl'>About Us</span></div>
+    <section className='border-2 box-border p-4 flex flex-col align-middle md:flex-row w-[95%] mx-auto md:h-[50vh] bg-gradient-to-t from-gray-700 to-gray-900'>
+        <div className='mx-auto block md:hidden my-3 md:my-2'><span className='text-white font-semibold font-sans text-4xl'>About Us</span></div>
         <div className='mx-auto w-full'>
             {/* <span className=''><iframe src={logo}  frameborder="0"></iframe></span> */}
-            <img className='h-32 sm:h-44  mx-auto' src={logo} alt="" />
+            <img className='h-32 sm:h-44 md:h-600 lg:h-80  mx-auto' src={logo} alt="" />
         </div>
         <main className='flex flex-col w-full md:mx-10'>
-        <div className='mx-auto hidden md:block my-2'><span className='text-white font-semibold font-sans text-3xl'>About Us</span></div>
-        <div className='mx-auto w-[95%]'><p className='text-center text-white font-sans font-medium text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus accusantium veniam exercitationem tempore fugiat beatae magnam, veritatis debitis ratione dolorum.</p>
+        <div className='mx-auto hidden md:block my-2'><span className='text-white font-semibold font-sans md:text-4xl lg:text-6xl'>About Us</span></div>
+        <div className='mx-auto w-[95%]'><p className='text-center text-white font-sans font-medium  text-lg md:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus accusantium veniam exercitationem tempore fugiat beatae magnam, veritatis debitis ratione dolorum.</p>
         </div>
         </main>
 
