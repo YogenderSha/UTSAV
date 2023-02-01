a6fcdb03fa58a813d938e7a989820f6d4c7b7540
nitin <nimble2905@gmail.com>
2023-02-01 14:41:43 +0530

    footer section of this project

---
+++ src/Components/Bottom.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Bottom() {
+  return (
+    <>
+      <footer className='w-[95%] mx-auto flex flex-col my-2 py-2 h-[20vh] border-2'>
+        <div className='mx-auto w-[95%] border-2 my-2 p-2 items-center flex '><span className=' border-2 border-green-300 mx-auto bg-red-300 w-fit text-white font-sans font-medium text-lg'>Give your Feedback here</span></div>
+        <section>
+
+        </section>
+      </footer>
+    </>
+  )
+}
