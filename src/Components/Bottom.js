2fb23131f8d9bb5d4e279aab695ab86db3c1ac3b
nitin <nimble2905@gmail.com>
2023-02-03 18:56:26 +0530

    modified

--- src/Components/Bottom.js
+++ src/Components/Bottom.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Bottom() {
   return (
     <>
-      <footer className='w-[95%] mx-auto flex flex-col my-2 py-2 h-[20vh] border-2'>
+      <footer className='w-[95%] mx-auto flex flex-col my-2 py-2 h-[20vh] border-2 bg-gradient-to-b from-gray-900 to-gray-700'>
         <div className='mx-auto w-[95%] border-2 my-2 p-2 items-center flex '><span className=' border-2 border-green-300 mx-auto bg-red-300 w-fit text-white font-sans font-medium text-lg'>Give your Feedback here</span></div>
         <section>
 
