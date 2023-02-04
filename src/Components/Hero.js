b28dcbe169b25f757d63297433df4cb30224186c
nitin <nimble2905@gmail.com>
2023-02-05 00:09:11 +0530

    some changes in the files

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <>
     <section className="bg-gradient-to-b from-gray-900 to-gray-800 h-[70vh]  w-[95%] mx-auto box-border p-2">
-      <main className="flex  flex-col align-middle border-2 border-gray-500 my-2 w-[95%] mx-auto items-center">
+      <main className="flex  flex-col align-middle border-gray-500 my-2 w-[95%] mx-auto items-center">
           <div className='my-6 lg:my-10'><h2 className='text-2xl lg:text-4xl font-sans font-bold text-white text-center'>Let us help to organize your events</h2></div>
           <div className='w-fit sm:w-2/3 box-border px-6 sm:px-0'><p className='text-white text-lg md:text-xl text-center font-sans font-medium sm:mx-4 md:mx-20 my-3'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tempore nisi molestiae suscipit odit non dolorum ex at possimus esse. Lorem, ipsum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, recusandae. . Aspernatur, nihil.</p></div>
           <div className='flex flex-row space-x-2 shadow my-4  rounded-sm bg-white '>
--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import './Scroll.css'
+import './Scroll.css';
+import logo from './hero.jpg';
 
 export default function Scroll() {
   return (
     <>
-    <section className=' flex h-[60vh] w-[95%] mx-auto my-2 border-2'>
+    <section className=' flex h-[60vh] w-[95%] mx-auto my-2 bg-gradient-to-t from-gray-900 to-gray-800'>
         {/* <div className='flex flex-col rotate-90 m-6 w-0'><span className='flex'>heading-1</span></div> */}
-        <main className='flex overflow-x-scroll justify-self-auto items-center my-2 box-border border-2 border-black  w-[80%] mx-auto bg-green-600'>
-            <div className="min-w-[400PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
-            <div className="min-w-[400PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
-            <div className="min-w-[400PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
-            <div className="min-w-[400PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
-            <div className="min-w-[400PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
-            <div className="min-w-[400PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
+        <main className='flex overflow-x-scroll justify-self-auto items-center my-2 box-border border-2 border-black  w-[100%] mx-auto bg-green-600'>
+            <div className="min-w-[300PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
+            <div className="min-w-[300PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
+            <div className="min-w-[300PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
+            <div className="min-w-[300PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
+            <div className="min-w-[300PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
+            <div className="min-w-[300PX] h-[50vh] rounded-md text-center bg-red-300 align-middle line-height mx-2">round box</div>
         </main>
         
     </section>
 
     <section className='flex flex-col w-[95%] mx-auto bg-gradient-to-t from-gray-700 my-4 border-2 border-green-400 box-border  to-gray-800  bg-red-900 md:h-[80vh]'>
           <div className=' inline mx-auto  md:block md:mx-10 my-3'>
-            <span className='text-white font-sans py-1 font-semibold text-4xl hover-underline-animation hover-underline-animation:after '>Heading</span>\
+            <span className='text-white font-sans py-1 font-semibold text-4xl'>Heading</span>
           </div>
 
           <main className='flex flex-col md:flex-row w-[95%] mx-auto h-full '>
-            <div className='flex flex-col md:mx-2  h-[40vh] md:h-[60vh] w-full my-2 py-2 border-2 rounded-md bg-gradient-to-b from-gray-800 to-gray-700'></div>
-            <div className='flex flex-col md:mx-2  h-[40vh] md:h-[60vh] w-full my-2 py-2 border-2 rounded-md bg-gradient-to-b from-gray-800 to-gray-700'></div>
-            <div className='flex flex-col md:mx-2  h-[40vh] md:h-[60vh] w-full my-2 py-2 border-2 rounded-md bg-gradient-to-b from-gray-800 to-gray-700'></div>
+            <div className='flex flex-col md:mx-2  h-[50vh] border-2 border-gray-600  w-full my-2 rounded-md bg-gradient-to-b from-gray-900 to-gray-800'>
+              <div>
+                <span><img className='rounded-sm w-fit sm:w-5/6' src={logo} alt="" /></span>
+
+              </div>
+            </div>
+            <div className='flex flex-col md:mx-2  h-[50vh] border-2 border-gray-600  w-full my-2 rounded-md bg-gradient-to-b from-gray-900 to-gray-800'>
+              <div>
+                <span><img className='rounded-sm w-fit sm:w-5/6' src={logo} alt="" /></span>
+
+              </div>
+            </div>
+            <div className='flex flex-col md:mx-2  h-[50vh] border-2 border-gray-600  w-full my-2 rounded-md bg-gradient-to-b from-gray-900 to-gray-800'>
+              <div>
+                <span><img className='rounded-sm w-fit sm:w-5/6' src={logo} alt="" /></span>
+
+              </div>
+            </div>
           </main>
          
         </section>
